e99c5bb2218b4d23c904d5edbcc421a216f8c80e
Joseph Dubon <josephdubon@pm.me>
2021-02-23 15:38:36 -0800

    Created custom ticket model.
    Took 19 minutes

--- bug_tracker/models.py
+++ bug_tracker/models.py
@@ -1,3 +1,25 @@
+"""
+Models with the appropriate fields
+Each ticket should have the following fields:
+
+Title
+Time / Date filed
+Description
+Name of user who filed ticket
+Status of ticket (New / In Progress / Done / Invalid)
+Name of user assigned to ticket
+Name of user who completed the ticket
+"""
+
+import datetime
+
 from django.db import models
 
-# Create your models here.
+
+# Ticket model
+class Ticket(models.Model):
+    title = models.CharField(max_length=150)
+    time_created = models.DateTimeField(default=datetime.datetime.now, blank=True)
+    description = models.TextField(max_length=350)
+    # look at comment above to finish model archetype
+    pass
