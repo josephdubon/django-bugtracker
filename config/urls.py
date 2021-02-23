aba671489d5b7ff654bf0701346d31aad32fc6a9
Joseph Dubon <josephdubon@pm.me>
2021-02-23 11:43:16 -0800

    Added homepage and accounts paths.
    Took 46 seconds

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,5 @@
-"""config URL Configuration
+"""
+config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -12,10 +13,24 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+    The URLs provided by auth are:
+
+        accounts/login/ [name='login']
+        accounts/logout/ [name='logout']
+        accounts/password_change/ [name='password_change']
+        accounts/password_change/done/ [name='password_change_done']
+        accounts/password_reset/ [name='password_reset']
+        accounts/password_reset/done/ [name='password_reset_done']
+        accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+        accounts/reset/done/ [name='password_reset_complete']
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
