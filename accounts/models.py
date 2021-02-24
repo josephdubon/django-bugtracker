35b13a3e4861ca459b64bb2b4ad5683c63bed503
Joseph Dubon <josephdubon@pm.me>
2021-02-23 17:11:18 -0800

    Updated custom user model. Added displayname and homepage + added them as required fields for createsuperuser.
    Took 45 seconds

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,15 @@
-from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
-# Custom User
+# Custom User Model
 class CustomUser(AbstractUser):
-    pass
+    displayname = models.CharField(max_length=50, null=True, blank=True)
+    homepage = models.URLField(null=True, blank=True)
+
+    # Add REQUIRED_FIELDS so fields will prompt on createsuperuser
+    REQUIRED_FIELDS = ['displayname', 'homepage']
 
     def __str__(self):
-        return self.username
+        return f'{self.username}{self.password}{self.displayname}{self.homepage}'
