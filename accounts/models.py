82bbe03f994809a155ec3554da03b834fc0e1cb4
Joseph Dubon <josephdubon@pm.me>
2021-02-23 16:13:30 -0800

    Created custom user model
    Took 21 seconds

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+# Custom User
+class CustomUser(AbstractUser):
+    pass
+
+    def __str__(self):
+        return self.username
