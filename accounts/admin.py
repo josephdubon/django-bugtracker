a2956218f45f836f338d1006e94a2bae48b52d5a
Joseph Dubon <josephdubon@pm.me>
2021-02-23 17:09:47 -0800

    Updated admin list_display, change user, and add user fields.
    Took 55 seconds

--- accounts/admin.py
+++ accounts/admin.py
@@ -9,7 +9,32 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username', ]
+
+    # Admin override users list view
+    list_display = [
+        'username',
+        'homepage',
+        'email',
+    ]
+
+    # Admin additional change user fields
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': (
+            'displayname',
+            'homepage',
+        )}),
+    )
+
+    # Admin additional add user fields
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': (
+            'displayname',
+            'homepage',
+            'email',
+        )}),
+    )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+admin.site.site_header = "Bug Tracker App"
