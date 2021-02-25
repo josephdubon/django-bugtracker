f1a83a30e5b4bc88ddbc0e70a101cb344973a275
Joseph Dubon <josephdubon@pm.me>
2021-02-24 18:07:51 -0800

    Completed the Ticket model, connected to author detail and ticket detail views.
    Took 1 minute

--- bug_tracker/models.py
+++ bug_tracker/models.py
@@ -1,25 +1,35 @@
 """
-Models with the appropriate fields
-Each ticket should have the following fields:
+Title: str
+Time / Date filed: datetime
+Description: str
+User who filed ticket: FK (Foreign Key)
+Status of ticket: str
+    Possible statuses
+    New
+    In Progress
+    Done
+    Invalid
+    hint: https://docs.djangoproject.com/en/3.0/ref/models/fields/#choices
+User assigned to ticket: FK
+User who completed the ticket: FK
 
-Title
-Time / Date filed
-Description
-Name of user who filed ticket
-Status of ticket (New / In Progress / Done / Invalid)
-Name of user assigned to ticket
-Name of user who completed the ticket
 """
 
-import datetime
-
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
 
 # Ticket model
 class Ticket(models.Model):
-    title = models.CharField(max_length=150)
-    time_created = models.DateTimeField(default=datetime.datetime.now, blank=True)
+    title = models.CharField(max_length=50)
     description = models.TextField(max_length=350)
-    # look at comment above to finish model archetype
-    pass
+    time_created = models.DateTimeField(default=timezone.now, blank=True)
+    author_of_ticket = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status_of_ticket = ''
+    # new, in_progress, done, invalid
+    assignee_of_ticket = ''
+    closer_of_ticket = ''
+
+    def __str__(self):
+        return f'{self.title} | {self.description} | {self.author_of_ticket}'
