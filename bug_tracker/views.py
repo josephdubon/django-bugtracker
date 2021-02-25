a0aada6df8bbf62fbbc638138d39f16d102a7457
Joseph Dubon <josephdubon@pm.me>
2021-02-24 18:19:11 -0800

    Created homepage view - connected to home.html at '/', ticket_detail view - connected to ticket_detail.html, and author_detail view - connected to author_detail.html.
    Took 4 minutes

--- bug_tracker/views.py
+++ bug_tracker/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from bug_tracker.models import Ticket
+
+
+# Home view
+def index(request):
+    tickets = Ticket.objects.all()
+    return render(request, 'home.html', {
+        'heading': 'Bug Tracker App',
+        'tickets': tickets,
+    })
+
+
+# Ticket detail view
+def ticket_detail(request, ticket_id):
+    ticket = Ticket.objects.get(id=ticket_id)
+    return render(request, 'ticket_detail.html', {
+        'ticket': ticket
+    }
+                  )
+
+
+# Author detail view
+def author_detail(request, author_id):
+    author_obj = Ticket.objects.get(author_of_ticket_id=author_id)
+    tickets = Ticket.objects.filter(author_of_ticket_id=author_id)
+    return render(request, 'author_detail.html', {
+        'author': author_obj.author_of_ticket,
+        'tickets': tickets
+    }
+                  )
