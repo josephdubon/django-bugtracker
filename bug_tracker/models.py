d65f38f60a770fe02ccf6e0d399a90db8c2848c0
Joseph Dubon <josephdubon@pm.me>
2021-02-26 13:11:28 -0800

    Implemented ticket status with tuple/list named TICKET_STATUS
    Took 32 seconds

--- bug_tracker/models.py
+++ bug_tracker/models.py
@@ -22,14 +22,23 @@ from django.utils import timezone
 
 # Ticket model
 class Ticket(models.Model):
+    TICKET_STATUS = [
+        ('New', 'New'),
+        ('In Progress', 'In Progress'),
+        ('Done', 'Done'),
+        ('Invalid', 'Invalid'),
+    ]
+
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=350)
     time_created = models.DateTimeField(default=timezone.now, blank=True)
-    author_of_ticket = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    status_of_ticket = ''
-    # new, in_progress, done, invalid
-    assignee_of_ticket = ''
-    closer_of_ticket = ''
+    status_of_ticket = models.CharField(max_length=24, choices=TICKET_STATUS, default='New')
+    author_of_ticket = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                                         related_name='author_of_ticket', null=True)
+    assignee_of_ticket = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True,
+                                           related_name='assignee_of_ticket')
+    closer_of_ticket = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True,
+                                         related_name='closer_of_ticket')
 
     def __str__(self):
         return f'{self.title} | {self.description} | {self.author_of_ticket}'
