2c179bc453173b10d3934d335f785b16baa40e7d
Joseph Dubon <josephdubon@pm.me>
2021-02-26 20:44:10 -0800

    Commented out the status field on edit_ticket. Can comment back in to change status through create_ticket form. forms.py and views.py.
    Took 8 minutes

--- bug_tracker/forms.py
+++ bug_tracker/forms.py
@@ -17,11 +17,4 @@ class EditTicketForm(forms.Form):
 
     title = forms.CharField(max_length=50)
     # status_of_ticket = forms.ChoiceField(choices=TICKET_STATUS)
-
     description = forms.CharField(max_length=350)
-    # author_of_ticket = forms.ForeignKey(settings.AUTH_USER_MODEL,
-    #                                     related_name='author_of_ticket', null=True)
-    # assignee_of_ticket = forms.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True,
-    #                                       related_name='assignee_of_ticket')
-    # closer_of_ticket = forms.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True,
-    #                                     related_name='closer_of_ticket')
