f461c7b442120c60aa31b4fc70e0a33a96466e6d
Joseph Dubon <josephdubon@pm.me>
2021-04-08 14:43:10 -0700

    Removed extra space

--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/static/'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Custom User Login Logout Redirect
